74a2c777c31aae8f05e9dea244f95e6c72b3c5cd
Miguel-Lopes17 <167643316+Miguel-Lopes17@users.noreply.github.com>
2024-11-21 09:24:41 -0300

    Add files via upload

---
+++ _inc/menu.php
@@ -0,0 +1,23 @@
+<nav class="navbar">
+        <div class="logo">
+            <img class="coin" src="./img/Coin Master - cor normal.png" alt="">
+            <h1>COINMASTER</h1>
+        </div>
+        <ul>
+            <li>
+                <a href="index.php">Inicio</a>
+            </li>
+            <li>
+                <a href="pags/sobre.html">Sobre</a>
+            </li>
+            <li>
+                <a href="pags/investimento.html">Comece a Investir</a>
+            </li>
+            <li>
+                <a href="">Serviços</a>
+            </li>
+            <li>
+                <a href="">Contato</a>
+            </li>
+        </ul>
+</nav>
